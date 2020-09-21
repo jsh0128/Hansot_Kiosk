6400782119c2cc60806837977c0c7aca7fb15b4b
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-09-21 15:47:28 +0900

    [ADD] Order.cs design
    주문하는 페이지 디자인 추가

--- Hansot_Kiosk/Order.cs
+++ Hansot_Kiosk/Order.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Hansot_Kiosk
 {
-    public partial class Form1 : Form
+    public partial class Order : Form
     {
-        public Form1()
+        public Order()
         {
             InitializeComponent();
         }
--- Hansot_Kiosk/Program.cs
+++ Hansot_Kiosk/Program.cs
@@ -16,7 +16,7 @@ namespace Hansot_Kiosk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Order());
         }
     }
 }
